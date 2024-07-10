8688e835bb600bdbd1c01946feb59bfb49bdb0f3
Rayhan F <rayhanfrdh123@gmail.com>
2024-07-10 19:31:29 +0800

    make migration

--- database/migrations/2024_07_07_122920_create_posts_table.php
+++ database/migrations/2024_07_07_122920_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->nullable();
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
